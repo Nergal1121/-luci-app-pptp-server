9af8b381fdaf654decd01c52ad130e339a7b1bce
Maxxx <nergal1121@gmail.com>
2025-06-17 23:51:41 +0300

    Update pptp-server.lua

--- dist/usr/lib/lua/luci/model/cbi/pptp-server/pptp-server.lua
+++ dist/usr/lib/lua/luci/model/cbi/pptp-server/pptp-server.lua
@@ -1,7 +1,11 @@
-mp = Map("pptpd", "PPTP Server", "")
+--require("luci.tools.webadmin")
+
+mp = Map("pptpd", "PPTP Server","")
 
 s = mp:section(NamedSection, "pptpd", "vpn", "PPTP Service")
-s.anonymous = true  -- Исправлено anonymouse -> anonymous
+s.anonymous = true  -- Исправлено: anonymouse -> anonymous
+
+--s.addremove = true
 
 s:option(Flag, "enabled", translate("Enable"))
 
@@ -11,9 +15,10 @@ localip.datatype = "ip4addr"
 remoteip = s:option(Value, "remoteip", translate("Remote IP"))
 remoteip.datatype = "string"
 
+
 logins = mp:section(TypedSection, "login", "PPTP Logins")
 logins.addremove = true
-logins.anonymous = true  -- Исправлено anonymouse -> anonymous
+logins.anonymous = true  -- Исправлено: anonymouse -> anonymous
 
 username = logins:option(Value, "username", translate("User name"))
 username.datatype = "string"
@@ -22,101 +27,119 @@ password = logins:option(Value, "password", translate("Password"))
 password.datatype = "string"
 
 function mp.on_save(self)
-    local cursor = luci.model.uci.cursor()
-    local have_pptp_rule = false
-    local have_gre_rule = false
+    require "luci.model.uci"
+    require "luci.sys"
 
-    cursor:foreach('firewall', 'rule', function(section)
-        if section.name == 'pptp' then
-            have_pptp_rule = true
-        elseif section.name == 'gre' then
-            have_gre_rule = true
+	local have_pptp_rule = false
+	local have_gre_rule = false
+
+    luci.model.uci.cursor():foreach('firewall', 'rule',
+        function (section)
+			if section._name == 'pptp' then
+				have_pptp_rule = true
+			end
+			if section._name == 'gre' then
+				have_gre_rule = true
+			end
         end
-    end)
+    )
 
-    if not have_pptp_rule then
-        local pptp_rule_name = cursor:add('firewall', 'rule')
-        cursor:set('firewall', pptp_rule_name, 'name', 'pptp')
-        cursor:set('firewall', pptp_rule_name, 'target', 'ACCEPT')
-        cursor:set('firewall', pptp_rule_name, 'src', 'wan')
-        cursor:set('firewall', pptp_rule_name, 'proto', 'tcp')
-        cursor:set('firewall', pptp_rule_name, 'dest_port', 1723)
-        cursor:save('firewall')
-        cursor:commit('firewall')
-    end
-
-    if not have_gre_rule then
-        local gre_rule_name = cursor:add('firewall', 'rule')
-        cursor:set('firewall', gre_rule_name, 'name', 'gre')
-        cursor:set('firewall', gre_rule_name, 'target', 'ACCEPT')
-        cursor:set('firewall', gre_rule_name, 'src', 'wan')
-        cursor:set('firewall', gre_rule_name, 'proto', 'gre')  -- Исправлено для протокола GRE
-        cursor:save('firewall')
-        cursor:commit('firewall')
-    end
+	if not have_pptp_rule then
+		local cursor = luci.model.uci.cursor()
+		local pptp_rule_name = cursor:add('firewall','rule')
+		cursor:tset('firewall', pptp_rule_name, {
+			['_name'] = 'pptp',
+			['target'] = 'ACCEPT',
+			['src'] = 'wan',
+			['proto'] = 'tcp',
+			['dest_port'] = 1723
+		})
+		cursor:save('firewall')
+		cursor:commit('firewall')
+	end
+	if not have_gre_rule then
+		local cursor = luci.model.uci.cursor()
+		local gre_rule_name = cursor:add('firewall','rule')
+		cursor:tset('firewall', gre_rule_name, {
+			['_name'] = 'gre',
+			['target'] = 'ACCEPT',
+			['src'] = 'wan',
+			['proto'] = 'gre'  -- Исправлено: dest_port -> proto
+		})
+		cursor:save('firewall')
+		cursor:commit('firewall')
+	end
+		
 end
 
--- Улучшенная проверка статуса сервиса
-function get_pptpd_status()
-    local status = {
-        running = (luci.sys.call("pidof pptpd >/dev/null") == 0),
-        enabled = nixio.fs.access("/etc/rc.d/S60pptpd")
-    }
-    
-    local msg = "PPTPD is "
-    msg = msg .. (status.running and "running" or "not running")
-    msg = msg .. " and "
-    msg = msg .. (status.enabled and "enabled" or "disabled")
-    msg = msg .. " on startup"
-    
-    return { status = msg }
+
+local pid = luci.util.exec("/usr/bin/pgrep pptpd")
+
+function pptpd_process_status()
+  local status = "PPTPD is not running now and "
+
+  if pid ~= "" then
+      status = "PPTPD is running with the PID " .. pid .. "and "
+  end
+
+  if nixio.fs.access("/etc/rc.d/S60pptpd") then
+    status = status .. "it's enabled on the startup"
+  else
+    status = status .. "it's disabled on the startup"
+  end
+
+  local status = { status=status }
+  local table = { pid=status }
+  return table
 end
 
-t = mp:section(Table, get_pptpd_status())
+t = mp:section(Table, pptpd_process_status())
 t.anonymous = true
+
 t:option(DummyValue, "status", translate("PPTPD status"))
 
-local status = get_pptpd_status()
-
-if not status.running then
-    start = t:option(Button, "_start", translate("Start"))
-    start.inputstyle = "apply"
-    function start.write(self, section)
-        local message = luci.util.exec("/etc/init.d/pptpd start 2>&1")
+if pid == "" then
+  start = t:option(Button, "_start", translate("Start"))
+  start.inputstyle = "apply"
+  function start.write(self, section)
+        message = luci.util.exec("/etc/init.d/pptpd start 2>&1")
+        luci.util.exec("sleep 4")
         luci.http.redirect(
-            luci.dispatcher.build_url("admin", "vpn", "pptp-server") ..
-            "?message=" .. luci.http.urlencode(message)
+                luci.dispatcher.build_url("admin", "vpn", "pptp-server") .. "?message=" .. message
         )
-    end
+  end
 else
-    stop = t:option(Button, "_stop", translate("Stop"))
-    stop.inputstyle = "reset"
-    function stop.write(self, section)
+  stop = t:option(Button, "_stop", translate("Stop"))
+  stop.inputstyle = "reset"
+  function stop.write(self, section)
         luci.util.exec("/etc/init.d/pptpd stop")
+        luci.util.exec("sleep 4")
         luci.http.redirect(
-            luci.dispatcher.build_url("admin", "vpn", "pptp-server")
+                luci.dispatcher.build_url("admin", "vpn", "pptp-server")
         )
-    end
+  end
 end
 
-if status.enabled then
-    disable = t:option(Button, "_disable", translate("Disable from startup"))
-    disable.inputstyle = "remove"
-    function disable.write(self, section)
+if nixio.fs.access("/etc/rc.d/S60pptpd") then
+  disable = t:option(Button, "_disable", translate("Disable from startup"))
+  disable.inputstyle = "remove"
+  function disable.write(self, section)
         luci.util.exec("/etc/init.d/pptpd disable")
+        luci.util.exec("sleep 1")
         luci.http.redirect(
-            luci.dispatcher.build_url("admin", "vpn", "pptp-server")
+                luci.dispatcher.build_url("admin", "vpn", "pptp-server")
         )
-    end
+  end
 else
-    enable = t:option(Button, "_enable", translate("Enable on startup"))
-    enable.inputstyle = "apply"
-    function enable.write(self, section)
+  enable = t:option(Button, "_enable", translate("Enable on startup"))
+  enable.inputstyle = "apply"
+  function enable.write(self, section)
         luci.util.exec("/etc/init.d/pptpd enable")
+        luci.util.exec("sleep 1")
         luci.http.redirect(
-            luci.dispatcher.build_url("admin", "vpn", "pptp-server")
+                luci.dispatcher.build_url("admin", "vpn", "pptp-server")
         )
-    end
+  end
 end
 
 return mp
