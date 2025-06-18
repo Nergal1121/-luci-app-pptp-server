4c81f2f36a599d68bee2bdb0e69c0c9337f643f8
Maxxx <nergal1121@gmail.com>
2025-06-18 20:53:23 +0300

    Update settings.lua

--- usr/lib/lua/luci/model/cbi/pptp-server/settings.lua
+++ usr/lib/lua/luci/model/cbi/pptp-server/settings.lua
@@ -1,76 +1,77 @@
-local s = require "luci.sys"
-local net = require"luci.model.network".init()
-local ifaces = s.net:devices()
-local m, s, o
+local sys = require "luci.sys"  -- Исправили имя переменной для sys
+local net = require "luci.model.network".init()
+local ifaces = sys.net:devices()  -- Исправили s.net на sys.net
+local m, section, o  -- Переименовали s в section для ясности
 m = Map("luci-app-pptp-server", translate("PPTP VPN Server"))
 m.description = translate("Simple, quick and convenient PPTP VPN, universal across the platform")
 m.template = "pptpd/index"
 
-s = m:section(TypedSection, "service")
-s.anonymous = true
+section = m:section(TypedSection, "service")
+section.anonymous = true
 
-o = s:option(DummyValue, "pptpd_status", translate("Current Condition"))
+o = section:option(DummyValue, "pptpd_status", translate("Current Condition"))
 o.template = "pptpd/status"
 o.value = translate("Collecting data...")
 
-o = s:option(Flag, "enabled", translate("Enable VPN Server"))
+o = section:option(Flag, "enabled", translate("Enable VPN Server"))
 o.rmempty = false
 
-o = s:option(Value, "localip", translate("Server IP"), translate("VPN Server IP address, it not required."))
+o = section:option(Value, "localip", translate("Server IP"), translate("VPN Server IP address, it not required."))
 o.datatype = "ipaddr"
 o.placeholder = translate("192.168.1.2")
 o.rmempty = true
 o.default = "192.168.1.2"
 
-o = s:option(Value, "remoteip", translate("Client IP"), translate("VPN Client IP address, it not required."))
+o = section:option(Value, "remoteip", translate("Client IP"), translate("VPN Client IP address, it not required."))
 o.placeholder = translate("192.168.1.10-20")
 o.rmempty = true
 o.default = "192.168.1.10-20"
 
---[[
-o = s:option(Value, "dns", translate("DNS IP address"), translate("This will be sent to the client, it not required."))
-o.placeholder = translate("192.168.1.1")
-o.datatype = "ipaddr"
-o.rmempty = true
-o.default = "192.168.1.1"
-]]--
-
-o = s:option(Flag, "mppe", translate("Enable MPPE Encryption"), translate("Allows 128-bit encrypted connection."))
+o = section:option(Flag, "mppe", translate("Enable MPPE Encryption"), translate("Allows 128-bit encrypted connection."))
 o.rmempty = false
 
--- Статус сервиса
-local pid = luci.util.exec("/usr/bin/pgrep pptpd")
-
-function pptpd_process_status()
-    local status = "PPTPD is not running now and "
+-- Статус сервиса (исправленная версия)
+local function get_pptpd_status()
+    local pid = luci.util.exec("/usr/bin/pgrep pptpd") or ""
+    local enabled = nixio.fs.access("/etc/rc.d/S60pptpd")
+    
+    local status_msg = "PPTPD is "
+    status_msg = status_msg .. (pid ~= "" and "running" or "not running")
+    status_msg = status_msg .. " and "
+    status_msg = status_msg .. (enabled and "enabled" or "disabled")
+    status_msg = status_msg .. " on startup"
     
     if pid ~= "" then
-        status = "PPTPD is running with the PID " .. pid .. " and "
+        status_msg = status_msg .. " (PID: " .. pid:gsub("\n", "") .. ")"
     end
     
-    if nixio.fs.access("/etc/rc.d/S60pptpd") then
-        status = status .. "it's enabled on the startup"
-    else
-        status = status .. "it's disabled on the startup"
-    end
-    
-    return { { status = status } }
+    return status_msg, pid, enabled
 end
 
-t = m:section(Table, pptpd_process_status())
-t.anonymous = true
-t:option(DummyValue, "status", translate("Service Status"))
+-- Секция статуса
+local status_section = m:section(Table, {{}})  -- Исправили на создание таблицы с одной пустой строкой
+status_section.anonymous = true
+
+-- Элемент статуса
+local status_value = status_section:option(DummyValue, "status", translate("Service Status"))
+status_value.rawhtml = true
+function status_value.cfgvalue()
+    local status_msg = get_pptpd_status()
+    return "<strong>" .. status_msg .. "</strong>"
+end
 
 -- Кнопки управления
+local status_msg, pid, enabled = get_pptpd_status()
+
 if pid == "" then
-    start = t:option(Button, "_start", translate("Start"))
+    start = status_section:option(Button, "_start", translate("Start"))
     start.inputstyle = "apply"
     function start.write(self, section)
         luci.util.exec("/etc/init.d/pptpd start")
         luci.http.redirect(luci.dispatcher.build_url("admin/vpn/pptpd/settings"))
     end
 else
-    stop = t:option(Button, "_stop", translate("Stop"))
+    stop = status_section:option(Button, "_stop", translate("Stop"))
     stop.inputstyle = "reset"
     function stop.write(self, section)
         luci.util.exec("/etc/init.d/pptpd stop")
@@ -78,19 +79,20 @@ else
     end
 end
 
-if nixio.fs.access("/etc/rc.d/S60pptpd") then
-    disable = t:option(Button, "_disable", translate("Disable from startup"))
+if enabled then
+    disable = status_section:option(Button, "_disable", translate("Disable from startup"))
     disable.inputstyle = "remove"
     function disable.write(self, section)
         luci.util.exec("/etc/init.d/pptpd disable")
         luci.http.redirect(luci.dispatcher.build_url("admin/vpn/pptpd/settings"))
     end
 else
-    enable = t:option(Button, "_enable", translate("Enable on startup"))
+    enable = status_section:option(Button, "_enable", translate("Enable on startup"))
     enable.inputstyle = "apply"
     function enable.write(self, section)
         luci.util.exec("/etc/init.d/pptpd enable")
         luci.http.redirect(luci.dispatcher.build_url("admin/vpn/pptpd/settings"))
     end
 end
+
 return m
